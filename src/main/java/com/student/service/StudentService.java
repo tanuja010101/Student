de11a5b107ae8d69e42714bd523e24c9798b4ea0
tparwani <parwanitanuja@gmail.com>
2022-11-24 23:23:34 +0530

    Added final update and post

--- src/main/java/com/student/service/StudentService.java
+++ src/main/java/com/student/service/StudentService.java
@@ -36,7 +36,6 @@ public class StudentService{
 
 
     public String deleteStudent(int rollNo) {
-       //student1.removeIf(x -> x.getRollNo() == (rollNo));
         for(int i=0;i<student1.size();i++){
             if(student1.get(i).getRollNo()==rollNo){
                 student1.remove(i);
@@ -53,6 +52,29 @@ public class StudentService{
             }
         }
         return null;
+    }
+    public Student updateStudent(Student student)
+    {
+        int idx=0;
+        int rollNo=0;
+        for( int i=0;i<student1.size();i++)
+        {
+            if(student1.get(i).getRollNo()==student.getRollNo())
+            {
+                rollNo=student.getRollNo();
+                idx=i;
+                break;
+            }
+
+        }
+        Student student2=new Student();
+        student2.setRollNo(rollNo);
+        student2.setStudentName(student.getStudentName());
+        student2.setGrade(student.getGrade());
+        student2.setAddress(student.getAddress());
+        student2.setDateOfBirth(student.getDateOfBirth());
+        student1.set(idx,student2);
+        return student2;
     }
 
 
